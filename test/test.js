52b67eed83e7c974d924e500ee42c6b34c1d1a40
Andrew Chilton <andychilton@gmail.com>
2015-07-07 22:43:15 +1200

    Run all three example site generations when testing

---
+++ test/test.js
@@ -0,0 +1,33 @@
+// --------------------------------------------------------------------------------------------------------------------
+
+'use strict'
+
+// core
+var fs = require('fs')
+
+// npm
+var test = require('tape')
+
+// local
+var seagull = require('..')
+
+// --------------------------------------------------------------------------------------------------------------------
+
+test('rebuild the average site', function(t) {
+
+  // use either the filename passed in, or the default 'config.json' for the config
+  var configFilename = 'config.json'
+
+  // read the config file
+  var cfg = fs.readFileSync(configFilename, 'utf8')
+  cfg = JSON.parse(cfg)
+
+  // pass the config to seagull
+  seagull(cfg, function(err) {
+    t.plan(2)
+    t.ok(!err, 'No error occurred when building the site')
+    t.pass('Site built without error')
+  })
+})
+
+// --------------------------------------------------------------------------------------------------------------------
